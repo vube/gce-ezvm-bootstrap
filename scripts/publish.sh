707cc323d0c0efb4186dd9ca794e2a5a10bd007a
Ross Perkins <rdp@accretivetg.com>
2014-10-07 19:08:34 -0700

    Fixed publish

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -15,23 +15,41 @@ GS_BOOTSTRAP_SH="__CONFIGURE_GS_BOOTSTRAP_URL__"
 ######################################################################
 # END CONFIGURATION SECTION
 ######################################################################
+#
+# DO NOT MODIFY ANYTHING BELOW HERE
+#
+
+if [ "$GS_EZVM_LOCAL_ARCHIVE" = "__CONFIGURE_GS_ARCHIVE_URL__" ]; then
+    echo "CONFIGURATION ERROR: You did not configure GS_EZVM_LOCAL_ARCHIVE in $0" 1>&2
+    exit 1
+fi
+
+if [ "$GS_BOOTSTRAP_SH" = "__CONFIGURE_GS_ARCHIVE_URL__" ]; then
+    echo "CONFIGURATION ERROR: You did not configure GS_BOOTSTRAP_SH in $0" 1>&2
+    exit 1
+fi
+
 
 cd "$(dirname $0)"
-BASE_DIR="$(pwd)"
+BASE_DIR="$(dirname $(pwd))"
 
 # Create the build dir if needed
 [ -d "$BASE_DIR/build" ] || mkdir -p "$BASE_DIR/build" || exit 1
 
-echo "# Building ezvm-local.tar.gz"
+archive=$(basename "$GS_EZVM_LOCAL_ARCHIVE")
 
-pushd local > /dev/null || exit 1
-tar -czf "$BASE_DIR/build/ezvm-local.tar.gz" * || exit 1
-popd > /dev/null
+echo "# Building $archive"
+
+cd "$BASE_DIR/local" || exit 1
+
+tar -czf "$BASE_DIR/build/$archive" * || exit 1
 
 # Copy files up to Google Cloud Storage
 
 echo "# Uploading to Google Cloud Storage"
 
+cd "$BASE_DIR" || exit 1
+
 gsutil cp scripts/bootstrap.sh "$GS_BOOTSTRAP_SH" || exit 1
 
-gsutil cp build/ezvm-local.tar.gz "$GS_EZVM_LOCAL_ARCHIVE" || exit 1
+gsutil cp "build/$archive" "$GS_EZVM_LOCAL_ARCHIVE" || exit 1
